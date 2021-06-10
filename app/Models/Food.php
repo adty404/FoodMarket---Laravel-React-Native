aedd1cc606735678d984b59529f6e861af998e2e
Aditya Prasetyo <adty404@gmail.com>
2021-06-10 07:45:59 +0700

    Initial Commit, Create Model, Create API Login & Register

---
+++ app/Models/Food.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class Food extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'price', 'rate', 'types', 'picturePath'
+    ];
+
+    public function toArray()
+    {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return $toArray;
+    }
+
+    public function getPicturePathAttribute()
+    {
+        return config('') . Storage::url($this->attributes['picturePath']);
+    }
+}
