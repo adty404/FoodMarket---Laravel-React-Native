a10975cb01c46f4b9431c5e0a705c17cb21e0ad4
Aditya Prasetyo <adty404@gmail.com>
2021-08-22 00:14:36 +0700

    Transaction View and Change Status

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -48,9 +48,11 @@ class TransactionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Transaction $transaction)
     {
-        //
+        return view('transactions.detail',[
+            'item' => $transaction
+        ]);
     }
 
     /**
@@ -85,5 +87,15 @@ class TransactionController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function changeStatus(Request $request, $id, $status)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $transaction->status = $status;
+        $transaction->save();
+
+        return redirect()->route('transactions.show', $id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::prefix('dashboard')->middleware(['auth:sanctum','admin'])->group(function
         Route::resource('users', UserController::class);
         Route::resource('food', FoodController::class);
 
+        Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])
+            ->name('transactions.changeStatus');
         Route::resource('transactions', TransactionController::class);
     });
 
